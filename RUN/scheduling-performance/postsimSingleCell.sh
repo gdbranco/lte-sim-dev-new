9e5d116d9e987399853475e7cf98a7e6796339d7
Guilherme David Branco <gdbranco@gmail.com>
2019-07-10 21:25:45 -0300

    Fix postSimSingleCell

--- RUN/scheduling-performance/postsimSingleCell.sh
+++ RUN/scheduling-performance/postsimSingleCell.sh
@@ -2,18 +2,18 @@ set -x
 set -e
 
 TOOLS="../../TOOLS"
-time=20
+time=100
 
 _NB_SCREENSHOT=10
 _COUNT=1
 until [ $_COUNT -gt $_NB_SCREENSHOT ]; do
-for sched in  1           #scheduling algorithm
+for sched in  1 2 3 4 5 6          #scheduling algorithm
 do
 for ue in 10 20 30 40 50        #number of users
 do
 for del in 0.1   #target delay
 do
-for v in 3                  #users speed
+for v in 30                  #users speed
 do
 
 	FILEIN="SCHED_${sched}_UE_${ue}_V_${v}_D_${del}_$_COUNT"
@@ -39,19 +39,6 @@ do
 		grep "RX VIDEO" TRACE/${FILEIN} |awk '{print $8}' >> tmp_gput_video
 		${TOOLS}/make_goodput tmp_gput_video $time >> ${TPUTVIDEO}
                                                        
-		grep -c "TX VOIP" TRACE/${FILEIN} > tmp_plr_voip
-		grep -c "RX VOIP" TRACE/${FILEIN} >> tmp_plr_voip
-		${TOOLS}/make_plr tmp_plr_voip | awk '{print $2}' | tail -1 >> ${PLRVOIP}
-
-		grep "RX VOIP" TRACE/${FILEIN} | awk '{print $14}' > tmp_delay_voip
-		${TOOLS}/make_avg tmp_delay_voip >> ${DELVOIP}
-
-		grep "RX VOIP" TRACE/${FILEIN} |awk '{print $8}' >> tmp_gput_voip
-		${TOOLS}/make_goodput tmp_gput_voip $time >> ${TPUTVOIP}
-
-		grep "RX INF_BUF" TRACE/${FILEIN} |awk '{print $8}' >> tmp_gput_be
-        ${TOOLS}/make_goodput tmp_gput_be ${time} >> ${TPUTBE}
-
 		rm TRACE/${FILEIN} 
 		rm tmp*
 done
