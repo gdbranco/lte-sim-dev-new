012e62a48a4b8d6357eabfc7181c179df07a25b6
Guilherme David Branco <gdbranco@gmail.com>
2019-08-17 10:47:05 -0300

    Fix web packet tags

--- src/protocolStack/rlc/am-rlc-entity.cpp
+++ src/protocolStack/rlc/am-rlc-entity.cpp
@@ -738,7 +738,7 @@ AmRlcEntity::CheckForDropPackets (double maxDelay, int bearerID)
 				   std::cout << " CBR";
 				 else if (amdRecord->m_packet->GetPacketTags()->GetApplicationType() == PacketTAGs::APPLICATION_TYPE_INFINITE_BUFFER)
 				   std::cout << " INF_BUF";
-				 else if (amdRecord->m_packet->GetPacketTags()->GetApplicationType == PacketTAGs::APPLICATION_TYPE_WEB)
+				 else if (amdRecord->m_packet->GetPacketTags()->GetApplicationType() == PacketTAGs::APPLICATION_TYPE_WEB)
 				   std::cout << " WEB";
 				 else
 				   std::cout << " UNKNOW";
